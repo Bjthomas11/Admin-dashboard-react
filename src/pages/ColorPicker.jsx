f4a79e0583218d71eaeee48485a0b5113ec3266d
Brian Thomas <brianthomas571@gmail.com>
2022-05-26 12:47:58 -0500

    added WYSIWYG editor

--- src/pages/ColorPicker.jsx
+++ src/pages/ColorPicker.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import {
+  GridComponent,
+  ColumnDirective,
+  ColumnsDirective,
+  Page,
+  Search,
+  Inject,
+  Toolbar
+} from "@syncfusion/ej2-react-grids";
+import { Header } from "../components";
 
 const ColorPicker = () => {
-  return <div>ColorPicker</div>;
+  return (
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-whate rounded-3xl">
+      <Header title="Color Picker" category="Apps" />
+    </div>
+  );
 };
 
 export default ColorPicker;
